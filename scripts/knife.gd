032a94142b4939fe4a0ee646160bfdd170b0bf30
Iaroslav Belozerov <iarbelozerov@gmail.com>
2026-01-05 17:54:28 +0100

    shits and giggles

---
+++ scripts/knife.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+@export var speed = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var collision = move_and_collide(Vector2(0, -speed * delta))
+	if collision:
+		var collider = collision.get_collider()
+		if collider.has_method("is_in_group") && collider.is_in_group("balloon"):
+			print("WAOW !!!")
